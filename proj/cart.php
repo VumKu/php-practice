6623ef7730bb123fd678a4fda07acd4cbc9c286d
Vum <ldxl3284@gmail.com>
2021-04-20 17:45:42 +0800

    0420practice

---
+++ proj/cart.php
@@ -0,0 +1,84 @@
+<?php include __DIR__ . '/part-text/HTML/config.php'; ?>
+
+<?php
+$title = '購物車';
+$pageName = 'cart';
+
+// if (empty($_SESSION['cart'])) {
+//     header('Location: product-list.php');
+//     exit;
+// }
+
+
+?>
+
+<?php include __DIR__ . '/part-text/HTML/html-head.php'; ?>
+<?php include __DIR__ . '/part-text/HTML/navbar.php'; ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <?php if (empty($_SESSION['cart'])) : ?>
+                <div class="alert alert-danger" role="alert">
+                    目前購物車裡沒有商品, 請至商品列表選購
+                </div>
+            <?php else : ?>
+                <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr>
+                            <th scope="col"><i class="fas fa-trash-alt"></i></th>
+                            <th scope="col">封面</th>
+                            <th scope="col">書名</th>
+                            <th scope="col">單價</th>
+                            <th scope="col">數量</th>
+                            <th scope="col">小計</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach ($_SESSION['cart'] as $v) : ?>
+                            <tr data-sid="<?= $v['sid'] ?>">
+                                <td>
+                                    <a href="javascript:" onclick="deleteItem(event)">
+                                        <i class="fas fa-trash-alt"></i>
+                                    </a>
+                                </td>
+                                <td>
+                                    <img src="<?= WEB_ROOT ?>/proj/imgs/small/<?= $v['book_id'] ?>.jpg" class="card-img-top" alt="">
+                                </td>
+                                <td><?= $v['bookname'] ?></td>
+                                <td><?= $v['price'] ?></td>
+                                <td><?= $v['quantity'] ?></td>
+                                <td><?= $v['price'] * $v['quantity'] ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            <?php endif; ?>
+
+        </div>
+    </div>
+</div>
+
+<?php include __DIR__ . '/part-text/HTML/scripts.php'; ?>
+<script>
+    function deleteItem(event) {
+        let me = $(event.currentTarget);
+        let sid = me.closest('tr').attr('data-sid');
+
+        $.get('cart-api.php', {
+            action: 'delete',
+            pid: sid
+        }, function(data) {
+            // location.reload();  // 刷頁面
+
+            showCartCount(data);
+            me.closest('tr').remove();
+
+            if ($('tbody>tr').length < 1) {
+                location.reload(); // 重新載入
+            }
+        }, 'json');
+
+    }
+</script>
+<?php include __DIR__ . '/part-text/HTML/html-foot.php'; ?>
